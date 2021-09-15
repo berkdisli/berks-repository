5b1b90647cd332b35cd4e1003b472f2dd60a28e8
Berk Disli <84175592+berkdisli@users.noreply.github.com>
2021-09-15 17:53:03 +0200

    css + js

---
+++ scripts.js
@@ -0,0 +1,14 @@
+alert('Hello world');
+
+let myFavoriteFood = 'Iskender Kebap';
+document.write(myFavoriteFood);
+
+/*let simpleAddition = 2 + 2;
+document.write(simpleAddition);
+
+let size = 100;
+let doubleSize = size * 2;
+let minSize = (doubleSize * 2) - (size / 2);
+document.write(doubleSize);
+
+document.write(minSize);*/
