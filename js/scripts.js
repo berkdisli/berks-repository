77c0c2d59d581e956e4a9a514da64e4cd689c90d
Berk Disli <berkdisli@yahoo.com>
2021-09-17 14:25:58 +0200

    pokedex

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,8 @@
-alert('Hello world');
-
-let myFavoriteFood = 'Iskender Kebap';
-document.write(myFavoriteFood);
-
-/*let simpleAddition = 2 + 2;
-document.write(simpleAddition);
-
-let size = 100;
-let doubleSize = size * 2;
-let minSize = (doubleSize * 2) - (size / 2);
-document.write(doubleSize);
-
-document.write(minSize);*/
+let pokemonList= [{name: "bulbasaur", type:["grass","poison"], height:"2'04\"", weight:"15.2 Ibs", abilities:"overgrow",category:"seed"},
+                  {name: "charmender", type:"fire", height:"2'00\"", weight:"18.7 Ibs", abilities:"blaze", category:"lizard"},
+                  {name: "pikachu", type:"electric", height:"1'04\"", weight:"13.2 Ibs", abilities:"static", category:"mouse"},
+                  {name: "squirtle", type:"water", height:"1'08\"", weight:"19.8 Ibs", abilities:"torrent", category:"tiny turtle"},
+                  {name: "butterfree", type:["bug","flying"], height:"3'07\"", weight:"70.5 Ibs", abilities:"compound eyes", category:"butterfly"},
+                  {name: "pidgeotto", type:["normal","flying"], height:"3'07\"", weight:"66.1 Ibs", abilities:["keen eye","tangled feet"], category:"bird"},
+                  {name: "primeape", type:"fighting", height:"3'03\"", weight:"70.5 Ibs", abilities:["vital spirit","anger point"], category:"pig monkey"},
+                  {name: "snorlax", type:"normal", height:"6'11\"", weight:"1014.1 Ibs", abilities:["thick fat","immunity"],category:"sleeping"}];
