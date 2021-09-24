14021b2220930f4a8e808840c8bdd6ad9dad64a7
Berk Disli <berkdisli@yahoo.com>
2021-09-24 10:39:20 +0200

    forEach instead of for loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,62 @@
+//let pokemonRepository = (function  () {
+
 let pokemonList=[
 {name: 'Bulbasaur', type:['grass','poison'], height:'2.04', weight:'15.2 Ibs', abilities:'overgrow',category:'seed'},
 {name: 'Charmender', type:'fire', height:'2.00', weight:'18.7 Ibs', abilities:'blaze', category:'lizard'},
 {name: 'Pikachu', type:'electric', height:'1.04', weight:'13.2 Ibs', abilities:'static', category:'mouse'},
 {name: 'Squirtle', type:'water', height:'1.08', weight:'19.8 Ibs', abilities:'torrent', category:'tiny turtle'},
-{name: 'Butterfree', type:['bug','flying'], height:'3.07', weight:'70.5 Ibs', abilities:'compound eyes', category:'butterfly'},
-{name: 'Pidgeotto', type:["normal","flying"], height: '3.07', weight:"66.1 Ibs", abilities:["keen eye","tangled feet"], category:"bird"},
-{name: "Primeape", type:"fighting", height:'3.03', weight:"70.5 Ibs", abilities:["vital spirit","anger point"], category:"pig monkey"},
-{name: "Snorlax", type:"normal", height:'6.11', weight:"1014.1 Ibs", abilities:["thick fat","immunity"],category:"sleeping"}
+{name: "Primeape", type:"fighting", height:'3.03', weight:"70.5 ", abilities:["vital spirit","anger point"], category:"pig monkey"},
+{name: "Snorlax", type:"normal", height:'6.11', weight:"1014.1 ", abilities:["thick fat","immunity"],category:"sleeping"}
 ];
 
+pokemonList.forEach(function(name){
+    document.write('<p>' + name.name + '</p>');
+  });
 
+//printArrayExample(pokemonList);
+//printArrayExample(pokemonList2);
+
+/*let pokemonList2=[
+{name: 'Butterfree', type:['bug','flying'], height:'3.07', weight:'70.5 ', abilities:'compound eyes', category:'butterfly'},
+{name: 'Pidgeotto', type:["normal","flying"], height: '3.07', weight:"66.1 ", abilities:["keen eye","tangled feet"], category:"bird"},
+{name: "Primeape", type:"fighting", height:'3.03', weight:"70.5 ", abilities:["vital spirit","anger point"], category:"pig monkey"},
+{name: "Snorlax", type:"normal", height:'6.11', weight:"1014.1 ", abilities:["thick fat","immunity"],category:"sleeping"}
+];*/
+
+/*function add(pokemon) {
+    pokemonList.push(pokemon);
+  }
+
+  function getAll() {
+    return pokemonList;
+  }
+
+  return {
+    add: add,
+    getAll: getAll
+  };
+})();
+document.write(pokemonRepository.getAll()); // []
+
+
+/*pokemonRepository.add({name: 'Pikachu' });
+document.write(pokemonRepository.getAll()); // [ { name: 'Pikachu' } ]
+
+
+
+let name = prompt('Please enter your name!');
+document.write(name);
+
+let isAccepted = confirm('Do you accept?');
+document.write(isAccepted);
+
+
+let array = [1,2,3,4];
+
+array.forEach(function(item,index,array) {
+document.write("item: " + item + " at index: " + index + " in the array: " + array);
+})
+/*function printArrayDetails(pokemonList){
 for (let i = 0; i < pokemonList.length; i++) {
 
   let pName = pokemonList[i].name;
@@ -25,9 +72,53 @@ for (let i = 0; i < pokemonList.length; i++) {
 
       else {
       document.write('<p>' + pName + ' ' + '(Height: ' + pHeight + ')' +' ' + "- It is a small Pokemon." + '</p>');
+  }
  }
 }
 
+function printArray(pokemonList2){
+for (let i = 0; i < pokemonList2.length; i++) {
+
+  let pName = pokemonList2[i].name;
+  let pWeight = pokemonList2[i].weight;
+
+      if (pWeight >= 300 ) {
+      document.write('<p>' + pName + ' ' + '(Weight: ' + pWeight + ')' +' ' + "- Wow, that's really heavy!" + '</p>');
+ }
+
+      else if (pWeight >=70  && pWeight<300 ){
+      document.write('<p>' + pName + ' ' + '(Weight: ' + pWeight + ')' +' ' + "- Average weight Pokemon." + '</p>');
+ }
+
+      else {
+      document.write('<p>' + pName + ' ' + '(Weight: ' + pWeight + ')' +' ' + "- It is light." + '</p>');
+  }
+ }
+}*/
+//printArrayDetails(pokemonList);
+//printArray(pokemonList2);
+
+
+ // executes the function using ‘pokemonList2‘ as its input
+
+/*function agag(firstName,lastName){
+  document.write("Hello , My name is  " + firstName + " " + lastName);
+}
+
+agag("Brad", "Pitt");
+
+function ShowMessage() {
+   console.log("Hello Ron!");
+   document.write("<br>Hello Ron!");
+}
+
+ShowMessage();*/
+
+
+
+// print();
+// print();
+// print();
 
 /*let text1 = "";
 let i = 0;
